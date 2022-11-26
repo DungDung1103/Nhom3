479c64a84d74f0c4ef15606f979affe69a992214
minhkham1511 <minhkham1511@gmail.com>
2022-11-26 23:36:08 +0700

    26.11

--- Models/Quanlysanpham.cs
+++ Models/Quanlysanpham.cs
@@ -5,11 +5,13 @@ namespace BTLNhom3.Models;
 public class Quanlysanpham
 {
     [Key]
+     [Required(ErrorMessage = "Mã  sản phẩm không được để trống !!!")]
     public string? Masanpham { get; set; }
     public string? Tensanpham { get; set; }
   
     public int size { get; set; }
      public int soluong { get; set; }
     public string? mausac { get; set; }
+    
     public decimal giatien { get; set; }
 }
